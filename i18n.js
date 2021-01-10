06f5062f6b47f32b846cec6d0f26a349a55ff9e8
atli <atli@ethicode.org>
2021-01-10 17:39:38 +0000

    german translations uncomplete

--- i18n.js
+++ i18n.js
@@ -3,13 +3,14 @@ const { localeSubpaths } = require('next/config').default().publicRuntimeConfig;
 const path = require('path');
 
 module.exports = new NextI18Next({
-  otherLanguages: ['is', 'fr', 'it', 'pt'],
+  otherLanguages: ['is', 'fr', 'it', 'pt', 'de'],
   localeSubpaths: {
     is: 'is',
     en: 'en',
     fr: 'fr',
     it: 'it',
     pt: 'pt',
+    de: 'de',
   },
   localePath: path.resolve('./public/static/locales'),
 });
